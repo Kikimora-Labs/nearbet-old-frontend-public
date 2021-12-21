3d877a75e7691d16f4c58060dfcbe47e3e409921
kirillarutyunov <kirill@arutyunov.me>
2021-12-21 10:10:41 +0000

    Deploying to master from @ Kikimora-Labs/nearbet-old-frontend@cbb5ba228d57ba9cd79305398dfc348043a3aa73 🚀

---
+++ precache-manifest.4d765a485f335e0ca0040997c543de63.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e850b5aea8ac94ed6f9676c2a8478b55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a46a5eaf54968be82e52",
+    "url": "/static/css/2.660505ec.chunk.css"
+  },
+  {
+    "revision": "b48ed607aa7ffbab0400",
+    "url": "/static/css/main.2e53c9fe.chunk.css"
+  },
+  {
+    "revision": "a46a5eaf54968be82e52",
+    "url": "/static/js/2.fd31b360.chunk.js"
+  },
+  {
+    "revision": "9c9ca057e4c77970bfa40d8d7962bfe6",
+    "url": "/static/js/2.fd31b360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48ed607aa7ffbab0400",
+    "url": "/static/js/main.0a4129ad.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "262a47421d048896215b4e82285927ad",
+    "url": "/static/media/believers.262a4742.svg"
+  },
+  {
+    "revision": "a043f265fae9c41d355181af666fce51",
+    "url": "/static/media/claimers.a043f265.svg"
+  },
+  {
+    "revision": "485b5e4cf5178ff4c2fe9d4ef2e167c6",
+    "url": "/static/media/croco.485b5e4c.png"
+  },
+  {
+    "revision": "ca0e49d1234f3b7c2a40345c5c55b243",
+    "url": "/static/media/croco2.ca0e49d1.png"
+  },
+  {
+    "revision": "21a77b0c622b4fa41a85b5d35d7ce296",
+    "url": "/static/media/founders.21a77b0c.svg"
+  },
+  {
+    "revision": "f87423937baf65a7e1692a62b4c86a34",
+    "url": "/static/media/lines.f8742393.png"
+  },
+  {
+    "revision": "485b5e4cf5178ff4c2fe9d4ef2e167c6",
+    "url": "/static/media/logo.485b5e4c.png"
+  }
+]);
